f4ca110ab31649f827896a22f6c55a0ce575eb71
Alex Hiller <asonnyhiller@gmail.com>
2022-09-14 22:37:46 -0400

    oh whoops i didnt make a branch

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -85,6 +85,10 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [60, 60]
         }
+      },
+      previewImage: {
+        type: DataTypes.STRING,
+        allowNull: true
       }
     },
     {
