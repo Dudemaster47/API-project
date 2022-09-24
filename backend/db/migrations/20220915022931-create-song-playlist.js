cf9bf0be29896bbcee27fef28ccdc620dc364b7d
Alex Hiller <asonnyhiller@gmail.com>
2022-09-24 14:10:48 -0400

    I spent the day your way

--- backend/db/migrations/20220915022931-create-song-playlist.js
+++ backend/db/migrations/20220915022931-create-song-playlist.js
@@ -9,10 +9,14 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       songId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {model: 'Songs',
+      key: 'id'}
       },
       playlistId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {model: 'Playlists',
+      key: 'id'}
       },
       createdAt: {
         allowNull: false,
